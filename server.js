14dad6c0f6046b4d8e541927157e0af8a32bda7c
unknown <matpet0412@gmail.com>
2022-03-02 19:16:15 +0200

    initialCommit4.5

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-dotenv.config({ path: './config/config.env'});
+dotenv.config();
 
 connectDB();
 
