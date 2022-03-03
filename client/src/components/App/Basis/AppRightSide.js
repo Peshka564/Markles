729e65d721e8b28d3c1197ecee8e516945b0ee37
Petar Petrov <matpet0412@gmail.com>
2022-03-03 16:37:50 +0200

    Polish

--- client/src/components/App/Basis/AppRightSide.js
+++ client/src/components/App/Basis/AppRightSide.js
@@ -8,7 +8,9 @@ const AppRightSide = ({setRetract}) => {
       <div className="d-flex flex-column w-100">
         <AppMainbar setRetract={setRetract}/>
         <div className='navline mt-2 mb-4 mx-auto'></div>
+        <div className='m-0 p-0'>
           <AppMain />
+        </div>
       </div>
   );
 };
