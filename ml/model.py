11e61984ca38c43a7c05b3322ef421b687c11163
Petar Petrov <matpet0412@gmail.com>
2022-03-10 21:03:49 +0200

    Final

--- ml/model.py
+++ ml/model.py
@@ -3,8 +3,8 @@ import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 import matplotlib.pyplot as plt
+import joblib
 
-import tensorflowjs as tfjs
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
@@ -15,9 +15,9 @@ def load_data():
     end_date = dt.datetime.now()
     yahoo_data = web.DataReader('FB', 'yahoo', start_date, end_date)
     yahoo_data = yahoo_data.resample('D').sum()
-    yahoo_data = (yahoo_data['Close'] * 3).values.reshape(-1, 1)
-    scaled_yahoo_data = scaler.fit_transform(yahoo_data)
-    return scaled_yahoo_data
+    yahoo_data = yahoo_data['Close'].values.reshape(-1, 1)
+    yahoo_data = scaler.fit_transform(yahoo_data)
+    return yahoo_data
 
 def split_data(data):
     train = data[: -250]
@@ -34,17 +34,18 @@ def segment_data(data, prediction_period):
 
 def build_model(x, y):
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x.shape[1], 1)))
+    model.add(LSTM(units=100, return_sequences=True, input_shape=(x.shape[1], 1)))
     model.add(Dropout(0.1))
-    model.add(LSTM(units=50, return_sequences=True))
+    model.add(LSTM(units=100, return_sequences=True))
     model.add(Dropout(0.1))
-    model.add(LSTM(units=50))
+    model.add(LSTM(units=100))
     model.add(Dropout(0.1))
     model.add(Dense(units=1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(x, y, epochs=50, batch_size=32)
-    model.save('test_model')
-    tfjs.converters.save_keras_model(model, "exportedModel")
+    model.fit(x, y, epochs=25, batch_size=32)
+
+    model.save('LSTM_model')
+    joblib.dump(scaler, "scaler.save")
     return model
 
 data = load_data()
@@ -62,6 +63,6 @@ test_values = all_values[len(train_data) - prediction_period:]
 test_x, test_y = segment_data(test_values, prediction_period)
 
 predictions = np.array(model.predict(test_x)).reshape(-1, 1)
-plt.plot(scaler.inverse_transform(test_data), color='blue', label="actual")
-plt.plot(scaler.inverse_transform(predictions), color='red', label="predicted")
+plt.plot(test_data, color='blue', label="actual")
+plt.plot(predictions, color='red', label="predicted")
 plt.show()
